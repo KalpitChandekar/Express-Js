9a55fb4bbcd34dc42a2a9beb1e042bd2d0b24158
Kalpit <kalpitchandekar1736@gmail.com>
2025-08-21 14:42:27 +0530

    add middleware

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use(function (req, res, next) {
+  console.log("Middleware working");
+  next();
+});
+
 app.get("/", function (req, res) {
   res.send("Hello World");
 });

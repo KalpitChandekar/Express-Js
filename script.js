44f4bc8f6d546b596261faa530040444aae4da23
Kalpit <kalpitchandekar1736@gmail.com>
2025-08-22 14:37:13 +0530

    error handling

--- script.js
+++ script.js
@@ -12,4 +12,16 @@ app.get("/site", function (req, res) {
   res.render("site", { heading: "PhoneHub" });
 });
 
+app.get("/error", function (res, req, next) {
+  throw new Error("Page not found!");
+});
+
+app.use(function errorHandler(err, req, res, next) {
+  if (res.headerSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render("error", { error: err });
+});
+
 app.listen(3000);
